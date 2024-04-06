92c450dd8e9dcea53299353ee6a5a4ffc2d1146e
Pranav Kale <107506777+PranavKale03@users.noreply.github.com>
2024-04-06 15:29:11 +0530

    Profile added

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,17 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../../context/UserContext";
+import ProfileImage from "../../assets/profile.svg";
+import EmailImage from "../../assets/Email.svg";
+import PhoneImage from "../../assets/Phone.svg";
+import { useNavigate } from "react-router-dom";
+import { clearAllLocalStorage } from "../../utils/localStorage";
 
 const Profile = () => {
   const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true)
+  const handleLogout = () => {
+    clearAllLocalStorage();
+    navigate("/");
+  };
   console.log(user);
+
   return (
-    <div className="mt-[100px] flex flex-col justify-center items-center">
+    <div className="mt-[150px] w-full h-full flex justify-center items-center">
       {user ? (
-        <>
-          <p>{user.fullName}</p>
-          <p>{user.role}</p>
-          <p>{user.type}</p>
-        </>
+        <div className="p-[100px] flex flex-col justify-center items-center gap-10 bg-blue-100 rounded-3xl">
+          <div className="flex flex-col justify-center items-center">
+            <div className="w-[150px] h-[150px] flex justify-center items-center rounded-full border border-black">
+              <img className="w-[110px]" src={ProfileImage} alt="Profile" />
+            </div>
+            <h1 className="font-bold text-[24px]">{user.fullName}</h1>
+            <p>{user.type}</p>
+          </div>
+          <div className="flex flex-col justify-center items-start gap-6">
+            <div className="flex justify-center items-center gap-4">
+              <img className="w-5" src={EmailImage} alt="Email" />
+              <p>{user.email}</p>
+            </div>
+            <div className="flex justify-center items-center gap-4">
+              <img className="w-5" src={PhoneImage} alt="Phone" />
+              <p>{user.mobile}</p>
+            </div>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="w-[200px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
       ) : (
         <>User not found.</>
       )}
